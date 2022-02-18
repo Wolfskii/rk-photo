4e9c5e0aa6713a3ce5794a3190d2a3de40bef74e
Dawid <dawid.kaleta@outlook.com>
2022-02-18 13:05:15 +0100

    Adding more form fields for album-item

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -4,38 +4,56 @@ import { useState, useEffect } from 'react'
 const axios = progressfy(_axios) // Adding progressify to axios
 
 export default function UploadForm () {
-  const [isUploading, setIsUploading] = useState(false)
-  const { images, setImages } = useState([])
+  // const [isUploading, setIsUploading] = useState(false)
+  const [images, setImages] = useState([])
 
   useEffect(() => {
-    // File Upload
-    function ekUpload () {
-      function Init () {
-        const fileSelect = document.getElementById('file-upload')
-        const fileDrag = document.getElementById('file-drag')
+    const fileSelect = document.getElementById('file-upload')
+    const fileDrag = document.getElementById('file-drag')
+    const submitBtn = document.getElementById('submit-album-btn')
 
-        fileSelect.addEventListener('change', async (e) => {
-          await fileSelectHandler(e)
-        }, false)
+    fileSelect.addEventListener('change', async (e) => {
+      await fileSelectHandler(e)
+    }, false)
 
-        // File Drop
-        fileDrag.addEventListener('dragover', fileDragHover, false)
-        fileDrag.addEventListener('dragleave', fileDragHover, false)
-        fileDrag.addEventListener('drop', async (e) => {
-          await fileSelectHandler(e)
-        }, false)
-      }
+    // File Drop
+    fileDrag.addEventListener('dragover', fileDragHover, false)
+    fileDrag.addEventListener('dragleave', fileDragHover, false)
+    fileDrag.addEventListener('drop', async (e) => {
+      const uploadedImages = await fileSelectHandler(e)
+      await setImages(uploadedImages)
+      console.log(uploadedImages)
+    }, false)
 
-      function fileDragHover (e) {
-        const fileDrag = document.getElementById('file-drag')
+    // Saving of album
+    submitBtn.addEventListener('click', async (e) => {
+      const album = await getAlbumFormData()
+      await saveAlbumOnline(album)
+    }, false)
+  }, [])
 
-        e.stopPropagation()
-        e.preventDefault()
+  const fileDragHover = (e) => {
+    const fileDrag = document.getElementById('file-drag')
 
-        fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload')
-      }
+    e.stopPropagation()
+    e.preventDefault()
 
-      async function fileSelectHandler (e) {
+    fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload')
+  }
+
+  // Testing one!!!
+  const fileSelectHandler = async (e) => {
+    const uploadedImages = ['test1', 'test2', 'test3']
+
+    // Cancel event and hover styling
+    fileDragHover(e)
+
+    return uploadedImages
+  }
+
+  /*         const fileSelectHandler = async (e) => {
+        const uploadedImages = []
+
         // Fetch FileList object
         const files = e.target.files || e.dataTransfer.files
 
@@ -48,138 +66,181 @@ export default function UploadForm () {
           parseFile(file)
 
           if (isSizeOkay(file)) {
-            await uploadImgToImgur(file)
+            const imgUrl = await uploadImgToImgur(file)
+            uploadedImages.push(imgUrl)
           } else {
             output('Bild-storlek för stor!')
           }
         }
-      }
 
-      // Output
-      function output (msg) {
-        // Response
-        const m = document.getElementById('messages')
-        m.innerHTML = msg
-      }
+        return uploadedImages
+      } */
 
-      function parseFile (file) {
-        output(
-          '<strong>' + encodeURI(file.name) + '</strong>'
-        )
+  // Output
+  const output = (msg) => {
+    // Response
+    const m = document.getElementById('messages')
+    m.innerHTML = msg
+  }
 
-        const imageName = file.name
+  const parseFile = (file) => {
+    output(
+      '<strong>' + encodeURI(file.name) + '</strong>'
+    )
 
-        const isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName)
+    const imageName = file.name
 
-        if (isGood) {
-          document.getElementById('start').classList.add('hidden')
-          document.getElementById('response').classList.remove('hidden')
-          document.getElementById('notimage').classList.add('hidden')
-          // Thumbnail Preview
-          document.getElementById('file-image').classList.remove('hidden')
-          document.getElementById('file-image').src = URL.createObjectURL(file)
-        } else {
-          document.getElementById('file-image').classList.add('hidden')
-          document.getElementById('notimage').classList.remove('hidden')
-          document.getElementById('start').classList.remove('hidden')
-          document.getElementById('response').classList.add('hidden')
-          document.getElementById('file-upload-form').reset()
-        }
-      }
+    const isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName)
 
-      function setProgressMaxValue (e) {
-        const pBar = document.getElementById('file-progress')
+    if (isGood) {
+      document.getElementById('start').classList.add('hidden')
+      document.getElementById('response').classList.remove('hidden')
+      document.getElementById('notimage').classList.add('hidden')
+      // Thumbnail Preview
+      document.getElementById('file-image').classList.remove('hidden')
+      document.getElementById('file-image').src = URL.createObjectURL(file)
+    } else {
+      document.getElementById('file-image').classList.add('hidden')
+      document.getElementById('notimage').classList.remove('hidden')
+      document.getElementById('start').classList.remove('hidden')
+      document.getElementById('response').classList.add('hidden')
+      document.getElementById('file-upload-form').reset()
+    }
+  }
 
-        if (e.lengthComputable) {
-          pBar.max = e.total
-        }
-      }
+  const setProgressMaxValue = e => {
+    const pBar = document.getElementById('file-progress')
 
-      function updateFileProgress (e) {
-        const pBar = document.getElementById('file-progress')
+    if (e.lengthComputable) {
+      pBar.max = e.total
+    }
+  }
 
-        if (e.lengthComputable) {
-          pBar.value = e.loaded
-        }
-      }
+  const updateFileProgress = e => {
+    const pBar = document.getElementById('file-progress')
 
-      async function uploadImgToImgur (imgFile) {
-        let imgUrl = ''
+    if (e.lengthComputable) {
+      pBar.value = e.loaded
+    }
+  }
 
-        const pBar = document.getElementById('file-progress')
+  const uploadImgToImgur = async imgFile => {
+    let imgUrl = ''
 
-        // Progress bar
-        pBar.style.display = 'inline'
+    const pBar = document.getElementById('file-progress')
 
-        // Start upload
-        const url = 'https://api.imgur.com/3/image'
-        const data = new FormData()
-        data.append('image', imgFile)
-        const config = {
-          headers: {
-            'Content-type': 'application/x-www-form-urlencoded',
-            Authorization: 'Client-ID ab4e03fd5059830'
-          }
-        }
-        console.log(imgFile)
-        imgUrl = await axios.post(url, data, config).data
-        console.log(imgUrl)
+    // Progress bar
+    pBar.style.display = 'inline'
 
-        return imgUrl
-      }
-
-      function isSizeOkay (file) {
-        const fileSizeLimit = 20 // In MB
-
-        if (file.size <= fileSizeLimit * 1024 * 1024) {
-          return true
-        } else {
-          return false
-        }
-      }
-
-      function browserHasFileSupport () {
-      // Check for the various File API support.
-        if (window.File && window.FileList && window.FileReader) {
-          return true
-        } else {
-          return false
-        }
-      }
-
-      if (browserHasFileSupport) {
-        Init()
-      } else {
-        document.getElementById('file-drag').style.display = 'none'
+    // Start upload
+    const url = 'https://api.imgur.com/3/image'
+    const data = new FormData()
+    data.append('image', imgFile)
+    const config = {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Client-ID ab4e03fd5059830'
       }
     }
 
-    ekUpload()
-  }, [isUploading])
+    const res = await axios.post(url, data, config)
+    imgUrl = res.data.data.link
+
+    return imgUrl
+  }
+
+  const isSizeOkay = file => {
+    const fileSizeLimit = 20 // In MB
+
+    if (file.size <= fileSizeLimit * 1024 * 1024) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const getAlbumFormData = () => {
+    const album = {
+      name: document.getElementById('album-name').value,
+      description: document.getElementById('album-desc').value,
+      category: document.getElementById('album-cat').value,
+      images: images
+    }
+
+    const datetime = document.getElementById('album-date').value
+
+    if (datetime !== '') {
+      album.datetime = datetime
+      // TODO: To datetime-format
+    }
+
+    return album
+  }
+
+  const saveAlbumOnline = async (album) => {
+    // Start upload
+    const url = 'http://localhost:3000/albums'
+
+    const data = {
+      name: album.name,
+      description: album.description,
+      category: album.category,
+      coverImgUrl: 'https://www.kamerabild.se/sites/kamerabild.se/files/styles/article_image/public/field/image/skarmavbild_2019-06-10_kl._08.25.30.png?itok=pPkJz20Q',
+      images: album.images
+    }
+
+    // Since datetime isn't obligatory and can be set automatically by API
+    if (album.datetime) {
+      data.datetime = album.datetime
+    }
+
+    const res = await axios.post(url, data)
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    const album = getAlbumFormData()
+    saveAlbumOnline(album)
+  }
 
   return (
-    <div>
+    <div id='upload-form'>
       <h2>Nytt album</h2>
 
-      {/* Upload  */}
-      <form id='file-upload-form' className='uploader'>
-        <input id='file-upload' type='file' name='fileUpload' accept='image/*' />
-        <label htmlFor='file-upload' id='file-drag'>
-          <img id='file-image' src='#' alt='Preview' className='hidden' />
-          <div id='start'>
-            <i className='fa fa-download' aria-hidden='true' />
-            <div>Select a file or drag here</div>
-            <div id='notimage' className='hidden'>Please select an image</div>
-            <span id='file-upload-btn' className='btn btn-primary'>Select a file</span>
-          </div>
-          <div id='response' className='hidden'>
-            <div id='messages' />
-            <progress className='progress' id='file-progress' value={0}>
-              <span>0</span>%
-            </progress>
-          </div>
-        </label>
+      <form onSubmit={submitForm}>
+        <label htmlFor='album-name'>Album-namn:</label>
+        <input type='text' id='album-name' name='album-name' />
+        <label htmlFor='album-desc'>Beskrivning:</label>
+        <textarea id='album-desc' name='album-desc' rows='10' cols='30' />
+        <label htmlFor='album-cat'>Kategori:</label>
+        <input type='text' id='album-cat' name='album-cat' />
+        <label htmlFor='album-date'>Datum:</label>
+        <input type='date' id='album-date' name='album-date' />
+
+        {/* Upload img */}
+        <div id='upload-form' className='uploader'>
+          <input id='file-upload' type='file' name='fileUpload' accept='image/*' />
+          <label htmlFor='file-upload' id='file-drag'>
+            <img id='file-image' src='#' alt='Preview' className='hidden' />
+            <div id='start'>
+              <i className='fa fa-download' aria-hidden='true' />
+              <div>Välj eller dra bilder hit</div>
+              <div id='notimage' className='hidden'>Välj bilder</div>
+              <span id='file-upload-btn' className='btn btn-primary'>Välj bilder</span>
+            </div>
+            <div id='response' className='hidden'>
+              <div id='messages' />
+              <progress className='progress' id='file-progress' value={0}>
+                <span>0</span>%
+              </progress>
+            </div>
+          </label>
+        </div>
+
+        <input id='submit-album-btn' type='submit' value='Spara' />
+
       </form>
+
     </div>
 
   )
