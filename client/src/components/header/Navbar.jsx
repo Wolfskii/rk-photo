0270aba8f1bee6f245b871781703a1e37cb37b8b
Dawid <dawid.kaleta@outlook.com>
2022-03-09 02:17:42 +0100

    Updating social links in header

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -6,13 +6,13 @@ export default function Navbar () {
     <nav>
       <ul>
         <li><Link to='/'>Hem</Link></li>
-        <li><Link to='/'>Om mig</Link></li>
-        <li><Link to='/'>Kontakt</Link></li>
+        <li><Link to='/om-mig'>Om mig</Link></li>
+        <li><Link to='/kontakt'>Kontakt</Link></li>
         <li><Link to='/'><img src={logo} alt='Logotyp' id='navbar-logo' /></Link></li>
-        <li><Link to='/'>Facebook</Link></li>
-        <li><Link to='/'>Instagram</Link></li><li><Link to='/'>E-post</Link></li>
+        <li><a href='https://www.facebook.com/renatakaleta66' target='_blank' rel='noreferrer'>Facebook</a></li>
+        <li><a href='https://www.instagram.com/renata1266' target='_blank' rel='noreferrer'>Instagram</a></li>
+        <li><a href='mailto:renatakaleta@hotmail.com?subject=RK-photo.se | '>E-post</a></li>
       </ul>
-
     </nav>
   )
 }
