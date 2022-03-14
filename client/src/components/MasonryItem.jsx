5a71cc41947405295ea9010665cacb3a17e1da16
Dawid <dawid.kaleta@outlook.com>
2022-03-14 20:55:46 +0100

    Breaking out to separate MasonryItem-stylesheet

--- client/src/components/MasonryItem.jsx
+++ client/src/components/MasonryItem.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
+import './MasonryItem.scss'
 
-export default function MasonryItem (imageSrc) {
+export default function MasonryItem(imageSrc) {
   const [modalIsHidden, setModalIsHidden] = useState(true)
 
   return (
     <div className='masonry-item' onClick={() => setModalIsHidden(!modalIsHidden)}>
       <img className='masonry-image' src={imageSrc.imageSrc} alt='img' />
-      <div className='modal' style={(modalIsHidden) ? { display: 'none' } : { display: 'block' }}>
-        <span className='modal-close-btn' onClick={() => setModalIsHidden(!modalIsHidden)}>×</span>
+      <div className='modal' style={modalIsHidden ? { display: 'none' } : { display: 'block' }}>
+        <span className='modal-close-btn' onClick={() => setModalIsHidden(!modalIsHidden)}>
+          ×
+        </span>
         <img src={imageSrc.imageSrc} className='modal-content' alt='modal-img' />
         <div className='modal-caption' />
       </div>
