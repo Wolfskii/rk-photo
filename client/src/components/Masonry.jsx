b93d8fc66e02f2fe5e4185c396c6973826d19cd1
Dawid <dawid.kaleta@outlook.com>
2022-03-14 20:50:20 +0100

    Breaking out to separate Masonry-stylesheet

--- client/src/components/Masonry.jsx
+++ client/src/components/Masonry.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Image from './MasonryItem'
+import './Masonry.scss'
 
 export default function Masonry() {
   const [images, setImages] = useState([])
