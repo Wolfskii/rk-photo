eee947fe6b8e082f7952543aec35298bce385d04
Dawid <dawid.kaleta@outlook.com>
2022-06-15 17:23:44 +0200

    Use Uppy as file uploader

---
+++ client/src/components/dashboard/ImageUploader.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from 'react'
+import Uppy from '@uppy/core'
+import XHRUpload from '@uppy/xhr-upload'
+import ImageEditor from '@uppy/image-editor'
+import { Dashboard } from '@uppy/react'
+import '@uppy/image-editor/dist/style.css'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+import './ImageUploader.scss'
+
+export default function ImageUploader({ ImgurClientID, onUpload }) {
+  const [images, setImages] = useState([])
+  let imgUrls = []
+
+  useEffect(() => {}, [])
+
+  const uppy = new Uppy({
+    autoProceed: false,
+    restrictions: {
+      maxFileSize: 20000000,
+      maxNumberOfFiles: 0,
+      minNumberOfFiles: 1,
+      allowedFileTypes: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+    }
+  })
+
+  uppy.use(ImageEditor, {
+    id: 'ImageEditor',
+    quality: 1,
+    cropperOptions: {
+      viewMode: 1,
+      background: false,
+      autoCropArea: 1,
+      responsive: true,
+      croppedCanvasOptions: {}
+    },
+    actions: {
+      revert: true,
+      rotate: true,
+      granularRotate: true,
+      flip: true,
+      zoomIn: true,
+      zoomOut: true,
+      cropSquare: true,
+      cropWidescreen: true,
+      cropWidescreenVertical: true
+    }
+  })
+
+  uppy.use(XHRUpload, {
+    endpoint: 'https://api.imgur.com/3/image',
+    method: 'post',
+    formData: false,
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Client-ID ${ImgurClientID}`
+    }
+  })
+
+  uppy.on('upload-success', (file, response) => {
+    /* const httpStatus = response.status // HTTP status code */
+    const httpBody = response.body // extracted response data
+    const uploadedImgLink = httpBody.data.link
+
+    // Do something with file and response
+    imgUrls.push(uploadedImgLink)
+    console.log(uploadedImgLink)
+  })
+
+  uppy.on('complete', (result) => {
+    // console.log('Upload complete! Uploaded files:', result.successful)
+    //console.log(result.successful)
+    // TODO: Skicka upp hela bild-arrayen till UploadForm
+
+    console.log(imgUrls)
+  })
+
+  return <Dashboard uppy={uppy} plugins={['ImageEditor']} />
+}
--- client/src/components/dashboard/UploadForm.jsx
+++ client/src/components/dashboard/UploadForm.jsx
@@ -1,161 +1,30 @@
 import _axios from 'axios'
 import progressfy from 'axios-progressfy'
-import { useState, useEffect } from 'react'
-const axios = progressfy(_axios) // Adding progressify to axios
+import { useState, useEffect } from 'react' // Adding progressify to axios
+import ImageUploader from './ImageUploader'
+const axios = progressfy(_axios)
 
-export default function UploadForm ({ token }) {
-  // const [isUploading, setIsUploading] = useState(false)
+export default function UploadForm({ token }) {
   const [images, setImages] = useState([])
 
   useEffect(() => {
-    const fileSelect = document.getElementById('file-upload')
-    const fileDrag = document.getElementById('file-drag')
-    const submitBtn = document.getElementById('submit-album-btn')
-
-    fileSelect.addEventListener('change', async (e) => {
-      await fileSelectHandler(e)
-    }, false)
-
-    // File Drop
-    fileDrag.addEventListener('dragover', fileDragHover, false)
-    fileDrag.addEventListener('dragleave', fileDragHover, false)
-    fileDrag.addEventListener('drop', async (e) => {
-      const uploadedImages = await fileSelectHandler(e)
-      await setImages(uploadedImages)
-    }, false)
-
-    // Saving of album
-    submitBtn.addEventListener('click', async (e) => {
-      const album = await getAlbumFormData()
-      await saveAlbumOnline(album)
-    }, false)
+    /*     const submitBtn = document.getElementById('submit-album-btn') */
+    /*     // Saving of album
+    submitBtn.addEventListener(
+      'click',
+      async (e) => {
+        const album = await getAlbumFormData()
+        await saveAlbumOnline(album)
+      },
+      false
+    ) */
   }, [])
-
-  const fileDragHover = (e) => {
-    const fileDrag = document.getElementById('file-drag')
-
-    e.stopPropagation()
-    e.preventDefault()
-
-    fileDrag.className = (e.type === 'dragover' ? 'hover' : 'modal-body file-upload')
-  }
-
-  // Testing one!!!
-  /*   const fileSelectHandler = async (e) => {
-    const uploadedImages = ['test1', 'test2', 'test3']
-
-    // Cancel event and hover styling
-    fileDragHover(e)
-
-    return uploadedImages
-  } */
-
-  const fileSelectHandler = async (e) => {
-    const uploadedImages = []
-
-    // Fetch FileList object
-    const files = e.target.files || e.dataTransfer.files
-
-    // Cancel event and hover styling
-    fileDragHover(e)
-
-    // Process all File objects
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i]
-      parseFile(file)
-
-      if (isSizeOkay(file)) {
-        const imgUrl = await uploadImgToImgur(file)
-        uploadedImages.push(imgUrl)
-      } else {
-        output('Bild-storlek för stor!')
-      }
-    }
-
-    return uploadedImages
-  }
 
   // Output
   const output = (msg) => {
     // Response
     const m = document.getElementById('messages')
     m.innerHTML = msg
-  }
-
-  const parseFile = (file) => {
-    output(
-      '<strong>' + encodeURI(file.name) + '</strong>'
-    )
-
-    const imageName = file.name
-
-    const isGood = (/\.(?=gif|jpg|png|jpeg)/gi).test(imageName)
-
-    if (isGood) {
-      document.getElementById('start').classList.add('hidden')
-      document.getElementById('response').classList.remove('hidden')
-      document.getElementById('notimage').classList.add('hidden')
-      // Thumbnail Preview
-      document.getElementById('file-image').classList.remove('hidden')
-      document.getElementById('file-image').src = URL.createObjectURL(file)
-    } else {
-      document.getElementById('file-image').classList.add('hidden')
-      document.getElementById('notimage').classList.remove('hidden')
-      document.getElementById('start').classList.remove('hidden')
-      document.getElementById('response').classList.add('hidden')
-      document.getElementById('file-upload-form').reset()
-    }
-  }
-
-  const setProgressMaxValue = e => {
-    const pBar = document.getElementById('file-progress')
-
-    if (e.lengthComputable) {
-      pBar.max = e.total
-    }
-  }
-
-  const updateFileProgress = e => {
-    const pBar = document.getElementById('file-progress')
-
-    if (e.lengthComputable) {
-      pBar.value = e.loaded
-    }
-  }
-
-  const uploadImgToImgur = async imgFile => {
-    let imgUrl = ''
-
-    const pBar = document.getElementById('file-progress')
-
-    // Progress bar
-    pBar.style.display = 'inline'
-
-    // Start upload
-    const url = 'https://api.imgur.com/3/image'
-    const data = new FormData()
-    data.append('image', imgFile)
-    const config = {
-      headers: {
-        'Content-type': 'application/x-www-form-urlencoded',
-        Authorization: 'Client-ID ab4e03fd5059830'
-      }
-    }
-
-    const res = await axios.post(url, data, config)
-    imgUrl = res.data.data.link
-
-    return imgUrl
-  }
-
-  const isSizeOkay = file => {
-    const fileSizeLimit = 20 // In MB
-
-    if (file.size <= fileSizeLimit * 1024 * 1024) {
-      return true
-    } else {
-      return false
-    }
   }
 
   const getAlbumFormData = async () => {
@@ -167,7 +36,7 @@ export default function UploadForm ({ token }) {
     }
 
     // Adding images to the array
-    images.forEach(image => {
+    images.forEach((image) => {
       album.images.push(image)
     })
 
@@ -211,6 +80,11 @@ export default function UploadForm ({ token }) {
     }
   }
 
+  function retrievedUploadedPics(imgUrls) {
+    setImages(imgUrls)
+    // TODO: Change state to next step in process
+  }
+
   const submitForm = async (e) => {
     e.preventDefault()
     const album = await getAlbumFormData()
@@ -218,7 +92,7 @@ export default function UploadForm ({ token }) {
   }
 
   return (
-    <div id='upload-form'>
+    <div id='main-form'>
       <h2>Nytt album</h2>
 
       <form onSubmit={submitForm}>
@@ -230,32 +104,13 @@ export default function UploadForm ({ token }) {
         <input type='text' id='album-cat' name='album-cat' />
         <label htmlFor='album-date'>Datum:</label>
         <input type='date' id='album-date' name='album-date' />
-
-        {/* Upload img */}
-        <div id='upload-form' className='uploader'>
-          <input id='file-upload' type='file' name='fileUpload' accept='image/*' />
-          <label htmlFor='file-upload' id='file-drag'>
-            <img id='file-image' src='#' alt='Preview' className='hidden' />
-            <div id='start'>
-              <i className='fa fa-download' aria-hidden='true' />
-              <div>Välj eller dra bilder hit</div>
-              <div id='notimage' className='hidden'>Välj bilder</div>
-              <span id='file-upload-btn' className='btn btn-primary'>Välj bilder</span>
-            </div>
-            <div id='response' className='hidden'>
-              <div id='messages' />
-              <progress className='progress' id='file-progress' value={0}>
-                <span>0</span>%
-              </progress>
-            </div>
-          </label>
-        </div>
-
         <input id='submit-album-btn' type='submit' value='Spara' />
-
       </form>
+      <ImageUploader ImgurClientID='ab4e03fd5059830' onUpload='' />
 
+      {images.map(function (image, i) {
+        return <img src={image} alt='somepic' />
+      })}
     </div>
-
   )
 }
