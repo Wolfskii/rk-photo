b867200bb3cc16f315fc4bbc95cc0cebdd3b5349
Dawid Kaleta <dawid.kaleta@outlook.com>
2022-11-22 23:31:05 +0100

    Isolate backend and frontend to two separate repos

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git subtree push --prefix server origin server
-echo "Server deployed!"
-
-#!/usr/bin/env node
-(cd client && npm run deploy);
-
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-(cd server && npm run start:dev) & (cd client/src && npm run start);
