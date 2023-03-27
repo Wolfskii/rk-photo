f829fddda3da1d0529c551b9b09740bbd6d44edd
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-03-27 17:23:32 +0200

    Optimize Albums.jsx

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,45 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import './Albums.scss'
 import Card from './AlbumCard'
 import Spinner from './Spinner'
 
-export default function Albums () {
+export default function Albums() {
   const [isLoaded, setIsLoaded] = useState(false)
   const [albums, setAlbums] = useState([])
+
+  // Fetch Albums
+  const fetchAlbums = useCallback(async () => {
+    const res = await fetch('https://api-rkphoto.cyclic.app/albums')
+    const data = await res.json()
+
+    return data.albums
+  }, [])
 
   useEffect(() => {
     const getAlbums = async () => {
       const albumsFromServer = await fetchAlbums()
-      await setAlbums(albumsFromServer)
+      setAlbums(albumsFromServer)
       setIsLoaded(true)
     }
 
-    window.localStorage.clear() // TODO: Fix better solution to check if token is not old
-
     getAlbums()
-  }, [])
-
-  // Fetch Albums
-  const fetchAlbums = async () => {
-    const res = await fetch('https://api-rkphoto.cyclic.app/albums')
-    const data = await res.json()
-
-    console.log('Anropat: https://api-rkphoto.cyclic.app/albums')
-
-    return data.albums
-  }
+  }, [fetchAlbums])
 
   if (!isLoaded) {
     return <Spinner />
   } else {
     return (
-      <>
-        <div className='albums'>
-          {albums.map((album, index) => (
-            <Card key={index} album={album} />
-          ))}
-        </div>
-      </>
+      <div className='albums'>
+        {albums.map((album, index) => (
+          <Card key={index} album={album} />
+        ))}
+      </div>
     )
   }
 }
