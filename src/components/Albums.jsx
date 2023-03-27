45be71f908710f1c583bf6cedad9582c029cdd18
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-03-27 17:32:25 +0200

    Comment code for clarity

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -3,11 +3,19 @@ import './Albums.scss'
 import Card from './AlbumCard'
 import Spinner from './Spinner'
 
+/**
+ * Albums component displays a collection of album cards fetched from an API.
+ * @returns {JSX.Element} - Rendered Albums component.
+ */
 export default function Albums() {
   const [isLoaded, setIsLoaded] = useState(false)
   const [albums, setAlbums] = useState([])
 
-  // Fetch Albums
+  /**
+   * Fetches the list of albums from the API.
+   * @callback fetchAlbums
+   * @returns {Promise<Array>} - A promise that resolves to an array of albums.
+   */
   const fetchAlbums = useCallback(async () => {
     const res = await fetch('https://api-rkphoto.cyclic.app/albums')
     const data = await res.json()
@@ -15,7 +23,13 @@ export default function Albums() {
     return data.albums
   }, [])
 
+  // Fetch the albums and set the state on component mount
   useEffect(() => {
+    /**
+     * Retrieves the albums by calling fetchAlbums and updates the component state.
+     * @async
+     * @function getAlbums
+     */
     const getAlbums = async () => {
       const albumsFromServer = await fetchAlbums()
       setAlbums(albumsFromServer)
@@ -25,12 +39,15 @@ export default function Albums() {
     getAlbums()
   }, [fetchAlbums])
 
+  // If the albums are not loaded yet, display the Spinner component
   if (!isLoaded) {
     return <Spinner />
   } else {
+    // Render the list of album cards once the data is loaded
     return (
       <div className='albums'>
         {albums.map((album, index) => (
+          // Render an AlbumCard component for each album in the list
           <Card key={index} album={album} />
         ))}
       </div>
